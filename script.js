4b5c1328141758bf3d377721595490c80954c740
Julian G <garicia321@gmail.com>
2025-08-09 16:12:12 -0500

    Se mejora codigo, se aplica CSS, se crea pagina sobre-nosotros, se agrega barrera de navegación con botones funcionales

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ botonesReserva.forEach((boton) => {
         window.location.href = "reservas.html";
     });
 });
+
+// Botón "Reservar Ahora"
+const botonReserva = document.querySelector(".btn-reserva");
+
+botonReserva.addEventListener("click", () => {
+    // Redirige a la página de reservas
+    window.location.href = "reservas.html";
+});
+
+// Botón "Sobre Nosotros"
+const botonsobrenosotros = document.querySelector(".btn-sobre-nosotros");
+
+if (botonsobrenosotros) {  // Solo si el botón existe
+    botonsobrenosotros.addEventListener("click", () => {
+        // Redirige a la página de sobre nosotros
+        window.location.href = "sobre-nosotros.html";
+    });
+}
+
